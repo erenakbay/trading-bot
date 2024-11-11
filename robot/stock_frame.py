bd0aac53f77b8b8cba2d3e121b1b83e226ffe1bd
Eren Akbay <erenakbay74@gmail.com>
2024-11-10 23:35:45 -0500

    stock fram update

--- robot/stock_frame.py
+++ robot/stock_frame.py
@@ -20,3 +20,33 @@ class Stockframe():
         self._frame = pd.DataFrame = self.create_frame()
         self._symbol_groups: DataFrameGroupBy = None
         self._symbol_rolling_groups: RollingGroupby = None
+
+    @property
+    def frame(self) -> pd.DataFrame:
+        return self._frame
+    
+    @property
+    def symbol_groups(self) -> DataFrameGroupBy:
+
+        self._symbol_groups = self._frame.groupby(
+            by='symbol',
+            as_index = False,
+            sort = True
+        )
+
+        return self.symbol_groups
+
+    def symbol_rolling_groups(self, size:int) -> RollingGroupby:
+        
+        if not self._symbol_groups:
+            self.symbol_groups
+
+        self._symbol_rolling_groups = self._symbol_groups.rolling(size)
+
+        return self._symbol_rolling_groups
+    
+    def create_frame(self) -> pd.DataFrame:
+
+        #Make a data frame
+        price_df = pd.DataFrame(data=self._data)
+        price_df = self._parse_datatime_column(price_df = price_df)
