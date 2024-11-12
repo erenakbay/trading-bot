feb7fafdb4a0be4164cddb941baff65354bd49d0
Eren Akbay <erenakbay74@gmail.com>
2024-11-11 22:15:26 -0500

    Add Indicators class for setting and retrieving stock indicator signals

---
+++ robot/indicator.py
@@ -0,0 +1,54 @@
+import operator
+import numpy as np
+import pandas as pd
+
+from typing import Any
+from typing import List
+from typing import Dict
+from typing import Union
+from typing import Optional
+from typing import Tuple
+
+from pyrobot.stock_frame import StockFrame
+
+class Indicators():
+
+    def __init__(self, price_data_frame: StockFrame) -> None:
+
+        self._stock_frame: StockFrame = price_data_frame
+        self._price_groups = price_data_frame.symbol_groups
+        self._current_indicators = {}
+        self._indicator_signals = {}
+        self._frame = self._stock_frame.frame
+
+    def set_indicator_signals(self, indicator: str, buy: float, sell: float, condition_buy: Any, condition_sell: Any) -> None:
+        # Set the buy and sell signals for the indicator
+        # This will allow us to create a signal for the strategy
+        # based on the indicator
+        # If there is no singal for that indicator set a template.
+        if indicator not in self._indicator_signals:
+            self._indicator_signals[indicator] = {}
+
+        # Modify the signal
+        self._indicator_signals[indicator]['buy'] = buy
+        self._indicator_signals[indicator]['sell'] = sell
+        self._indicator_signals[indicator]['buy_operator'] = condition_buy
+        self._indicator_signals[indicator]['sell_oeprator'] = condition_sell
+
+    def get_indicator_signals(self, indicator: Optional[str]) -> Dict:
+
+        if indicator and indicator in self._indicator_signals:
+            return self._indicator_signals[indicator]
+        else:
+            # Returns the indicator signals
+            return self._indicator_signals
+        
+    @property
+    def price_data_frame(self) -> pd.DataFrame:
+
+        return self._frame
+
+    @price_data_frame.setter
+    def price_data_frame(self, price_data_frame: pd.DataFrame) -> None:
+
+        self._frame = price_data_frame
