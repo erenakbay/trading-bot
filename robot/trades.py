1114e3482e4f041df92ced1b01a58fd61ba80c99
Eren Akbay <erenakbay74@gmail.com>
2024-11-14 22:14:54 -0500

    Modify side model, added instrument and make good_till_cancel.

--- robot/trades.py
+++ robot/trades.py
@@ -63,6 +63,9 @@ class Trade():
         if self.order['orderType'] == 'STOP':
             self.order['stopPrice'] = price
 
+        if self.order['orderType'] == 'LIMIT':
+            self.order['price'] = price
+
         elif self.order['orderType'] == 'STOP_LIMIT':
             self.order['stopPrice'] = price
             self.order['price'] = stop_limit_price
@@ -72,3 +75,68 @@ class Trade():
             self.order['stopPriceLinkType'] = ""
             self.order['stopPriceOffset'] = 0.00
             self.order['stopType'] = "STANDARD"
+
+        self.enter_or_exit = enter_or_exit
+        self.side = side
+        self.order_types = order_type
+        self.price = price
+
+        # Store the order
+        if order_type == 'stop':
+            self.stop_price = price
+        elif order_type == 'stop-lmt':
+            self.stop_price = price
+            self.stop_limit_price = stop_limit_price
+        else:
+            self.stop_price = 0.00
+
+        # Set the opposite side
+        if self.enter_or_exit == 'enter':
+            self.enter_or_exit_opposite = 'exit'
+        elif self.enter_or_exit == 'exit':
+            self.enter_or_exit_opposite = 'enter'
+
+        if self.side == 'long':
+            self.side_opposite = 'short'
+        elif self.side == 'short':
+            self.side_opposite == 'long'
+        return self.order
+    
+    def instrument(self, symbol: str, quantity: int, asset_type: str, sub_asset_type: str = None, order_leg_id: int = 0) -> dict:
+
+        leg = self.order['orderLegCollection'][order_leg_id]
+
+        leg['instument']['symbol'] = symbol
+        leg['instument']['assetType'] = asset_type
+        leg['quantity'] = quantity
+
+        self.order_size = quantity
+        self.symbol = symbol
+        self.asset_type = asset_type
+        
+        return leg
+    
+    def good_till_cancel(self, cancel_time: datetime) -> None:
+
+        self.order['duration'] = 'GOOD_TILL_CANCEL'
+        self.order['cancelTime'] = cancel_time.isoformat()
+
+    def modify_side(self, side: Optional[str], order_leg_id: int = 0) -> None:
+
+        if side and side not in ['buy', 'sell', 'sell_short', 'buy_to_cover']:
+            raise ValueError('You passed the wrong side. Please use buy, sell, sell_short, or buy_to_cover')
+        
+        if side:
+            self.side['orderLegCollection'][order_leg_id]['instruction'] = side.upper()
+        else:
+            self.order['orderLegCollection'][order_leg_id]['instruction'] = self.order_instructions[self.enter_or_exit][self.side_opposite]
+
+    def add_box_range(self, profit_size: float = 0.00, percentage: bool = False, stop_limit: bool = False):
+
+        if not self._triggered_added:
+            self._convert_to_trigger()
+
+        self.add_Take_profit(profit_size = profit_size, percentage = percentage)
+
+        if not stop_limit:
+            self.add_stop_loss(stop_size = profit_size, percentage = percentage)
